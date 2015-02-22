2f5ab2198e296e9c5ac36db3ff9884da04ce8720
Ionică Bizău <bizauionica@yahoo.com>
2015-02-22 15:04:12 +0200

    Initial commit.

--- test/index.js
+++ test/index.js
@@ -7,11 +7,15 @@ var Repo = require("../lib")
 var myRepo = new Repo("./foo");
 myRepo.create(function (err) {
     if (err) throw err;
+    console.log("> Created the repository.");
     Fs.writeFileSync("./foo/README", "Hello World!");
+    console.log("> Created README.");
     myRepo.exec("add .", function (err) {
         if (err) throw err;
+        console.log("> Added the files.");
         myRepo.commit("Initial commit.", function (err) {
             if (err) throw err;
+            console.log("> Created the initial commit.");
         });
     });
 });
