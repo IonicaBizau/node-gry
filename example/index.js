37016b2d160bcd631e15c0769dc42b71682f400b
Ionică Bizău <bizauionica@gmail.com>
2016-02-19 14:38:54 +0200

    Rewritten in ES2015.

--- example/index.js
+++ example/index.js
@@ -1,26 +1,28 @@
+"use strict";
+
 // Dependencies
-var Repo = require("../lib")
-  , Fs = require("fs")
-  , OneByOne = require("one-by-one")
-  ;
+const Repo = require("../lib")
+    , fs = require("fs")
+    , oneByOne = require("one-by-one")
+    ;
 
 // Create the repository
 var myRepo = new Repo("./foo");
-OneByOne([
+oneByOne([
     myRepo.create.bind(myRepo)
-  , function (cb) {
+  , cb => {
         console.log("> Created the repository.");
-        Fs.writeFile("./foo/README", "Hello World!", cb);
+        fs.writeFile("./foo/README", "Hello World!", cb);
     }
-  , function (cb) {
+  , cb => {
         console.log("> Created README.md");
         myRepo.exec("add .", cb);
     }
-  , function (cb) {
+  , cb => {
         console.log("> Added the files.");
         myRepo.commit("Initial commit.", cb);
     }
-  , function (cb) {
+  , cb => {
         console.log("> Created the initial commit.");
         cb();
     }
